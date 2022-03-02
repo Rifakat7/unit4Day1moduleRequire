5e497c13b1169d3d16bf3a537614e57acd50a955
Rifakat7 <97470134+Rifakat7@users.noreply.github.com>
2022-03-02 22:14:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const add = require("./add")
+
+const divide = require("./divide")
+
+const multiply = require("./multiply")
+
+const sub = require("./subtract")
+
+
+var a=150;
+var b = 50;
+
+console.log("Addition of a and b is",add(a,b));
+console.log("division of a and b is",divide(a,b));
+console.log("multiply of a and b is",multiply(a,b));
+console.log("subtraction of a and b is",sub(a,b));
